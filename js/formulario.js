842c70e38dbff0f1fd24662aca4e80464ed3db3b
Luis Herrera <luisskater09@gmail.com>
2022-03-27 21:38:58 -0500

    Previniendo default de enviar y borrando insputs de formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,5 +1,14 @@
 const btnEnviar = document.getElementById("btn-enviar");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const textArea = document.getElementById("text-area");
 
 btnEnviar.addEventListener('click', e =>{
     e.preventDefault();
+
+    nombre.value = ""
+    apellido.value = ""
+    email.value = ""
+    textArea.value = ""
 })
