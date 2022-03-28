dca285e51f2947677d0d31bf4dc7c4746820463c
Luis Herrera <luisskater09@gmail.com>
2022-03-27 21:46:42 -0500

    Agregando prevet default del buton del formulario

--- js/formulario.js
+++ js/formulario.js
@@ -5,10 +5,10 @@ const email = document.getElementById("email");
 const textArea = document.getElementById("text-area");
 
 btnEnviar.addEventListener('click', e =>{
-    // e.preventDefault();
+    e.preventDefault();
 
-    nombre.value = ""
-    apellido.value = ""
-    email.value = ""
-    textArea.value = ""
+    // nombre.value = ""
+    // apellido.value = ""
+    // email.value = ""
+    // textArea.value = ""
 })
