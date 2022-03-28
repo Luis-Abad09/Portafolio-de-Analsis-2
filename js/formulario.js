5f4c8ceae88344ef1f8388c4eb611192e221595b
Luis Herrera <luisskater09@gmail.com>
2022-03-27 21:53:07 -0500

    Quitando(comentando) archivo formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -1,11 +1,11 @@
 const btnEnviar = document.getElementById("btn-enviar");
-const nombre = document.getElementById("nombre");
-const apellido = document.getElementById("apellido");
-const email = document.getElementById("email");
-const textArea = document.getElementById("text-area");
+// const nombre = document.getElementById("nombre");
+// const apellido = document.getElementById("apellido");
+// const email = document.getElementById("email");
+// const textArea = document.getElementById("text-area");
 
 btnEnviar.addEventListener('click', e =>{
-    e.preventDefault();
+    e.preventDefault(); 
 
     // nombre.value = ""
     // apellido.value = ""
